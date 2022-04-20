1f0a5ea95a7827a1229a2d3bbb5205d3410476e7
lucienxixi <liuanxi0528@outlook.com>
2022-04-20 01:29:42 -0400

    add new solution

---
+++ Algorithms/monotonous_stack/lc2104/Solution.java
@@ -0,0 +1,104 @@
+// brute force 
+// O(n^2)
+class Solution1 {
+    public long subArrayRanges(int[] nums) {
+        long sum = 0L;
+        for (int i = 0; i < nums.length; i++) {
+            int min = nums[i];
+            int max = nums[i];
+            for (int j = i; j >= 0; j--) {
+                min = Math.min(min, nums[j]);
+                max = Math.max(max, nums[j]);
+                sum += max - min;
+            }
+        }
+        return sum;
+    }
+}
+
+// https://leetcode.com/problems/sum-of-subarray-ranges/discuss/1626628/O(n)-solution-with-monotonous-stack-oror-Full-explaination
+
+// monotonous stack, based on 907. Sum of Subarray Minimums
+// O(n)
+
+public class Solution {
+    public long subArrayRanges(int[] nums) {
+        Stack<Integer> stack = new Stack<>();
+        long[] nextMin = new long[nums.length];
+        long[] prevMin = new long[nums.length];
+        long[] nextMax = new long[nums.length];
+        long[] prevMax = new long[nums.length];
+        
+        for (int i = nums.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && nums[stack.peek()] > nums[i]) {
+                stack.pop();
+            }
+            
+            if (stack.isEmpty()) {
+                nextMin[i] = nums.length;
+            } else {
+                nextMin[i] = stack.peek();
+            }
+            
+            stack.push(i);
+        }
+        
+        stack.clear();
+        
+        for (int i = 0; i < nums.length; i++) {
+            while (!stack.isEmpty() && nums[stack.peek()] >= nums[i]) {
+                stack.pop();
+            }
+            
+            if (stack.isEmpty()) {
+                prevMin[i] = -1;
+            } else {
+                prevMin[i] = stack.peek();
+            }
+            
+            stack.push(i);
+        }
+        
+        stack.clear();
+        
+        for (int i = nums.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && nums[stack.peek()] < nums[i]) {
+                stack.pop();
+            }
+            
+            if (stack.isEmpty()) {
+                nextMax[i] = nums.length;
+            } else {
+                nextMax[i] = stack.peek();
+            }
+            
+            stack.push(i);
+        }
+        
+        stack.clear();
+        
+        for (int i = 0; i < nums.length; i++) {
+            while (!stack.isEmpty() && nums[stack.peek()] <= nums[i]) {
+                stack.pop();
+            }
+            
+            if (stack.isEmpty()) {
+                prevMax[i] = -1;
+            } else {
+                prevMax[i] = stack.peek();
+            }
+            
+            stack.push(i);
+        }
+        
+        
+        long sum = 0L;
+        
+        for (int i = 0; i < nums.length; i++) {            
+            sum += nums[i] * ((nextMax[i] - i) * (i - prevMax[i]) - (nextMin[i] - i) * (i - prevMin[i]));    
+        }
+        
+
+        return sum;
+    }
+}
---
+++ Algorithms/monotonous_stack/lc907/Solution.java
@@ -0,0 +1,56 @@
+// based on LC496 Next Greater Element 1
+// monotonous stack + math
+// montonous stack: to find the nearest nextMin and the nearest prevMin
+// math: acccording to the index of nextMin and the index of prevMin to calculate the number of subarray between nextMin and prevMin. number = (i - index of prevMin) * (index of nextMin - i), sum = number * arr[i], which means the min of all of the subarrays between nextMin and prevMin is arr[i]
+
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        long[] nextMin = new long[arr.length];
+        long[] prevMin = new long[arr.length];
+        
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
+                stack.pop();
+            }
+            
+            if (stack.isEmpty()) {
+                nextMin[i] = arr.length;
+            } else {
+                nextMin[i] = stack.peek();
+            }
+            
+            stack.push(i);
+        }
+        
+        
+        stack.clear();
+        
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+                stack.pop();
+            }
+            
+            if (stack.isEmpty()) {
+                prevMin[i] = -1;
+            } else {
+                prevMin[i] = stack.peek();
+            }
+            
+            stack.push(i);
+        }
+        
+        // System.out.println(Arrays.toString(nextMin));
+        // System.out.println(Arrays.toString(prevMin));
+        
+        long sum = 0L;
+        int mod = 1_000_000_007;
+        
+        for (int i = 0; i < arr.length; i++) {            
+            sum += arr[i] * (nextMin[i] - i) * (i - prevMin[i]);    
+        }
+        
+        long ans = sum % mod;
+        return (int)ans;
+    }
+}
