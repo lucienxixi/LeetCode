90ce29e7178d84e5f0a51e67f8d509d48a8079aa
lucienxixi <liuanxi0528@outlook.com>
2022-05-01 02:04:58 -0400

    add new solutions

---
+++ Algorithms/greedy/lc1481/Solution.java
@@ -0,0 +1,54 @@
+// 1481. Least Number of Unique Integers after K Removals
+
+
+// 看了tag，solved by myself
+
+// hashmap + sort
+class Solution {
+    public int findLeastNumOfUniqueInts(int[] arr, int k) {
+        Map<Integer, Integer> numToCnt = new HashMap<>();
+        for (int num : arr) {
+            numToCnt.put(num, numToCnt.getOrDefault(num, 0) + 1);
+        }
+        
+        Integer[] nums = numToCnt.keySet().toArray(new Integer[0]);
+        Arrays.sort(nums, (n1, n2) -> Integer.compare(numToCnt.get(n1), numToCnt.get(n2)));
+        
+        int ans = nums.length;
+        
+        for (int num : nums) {
+            if (numToCnt.get(num) == k) {
+                ans--;
+                return ans;
+            }  else if (numToCnt.get(num) < k) {
+                k -= numToCnt.get(num);
+                ans--;
+            } else {
+                return ans;
+            }
+        }
+        
+        return 0;
+    }
+}
+
+// heap
+class Solution1 {
+    public int findLeastNumOfUniqueInts(int[] arr, int k) {
+        
+    }        
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
